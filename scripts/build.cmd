212f25c0c47e65e7b2bf7b081ae9fc7b6c4652d2
jkidwell <jay@contensive.com>
2025-06-02 17:55:42 -0400

    added portal

--- scripts/build.cmd
+++ scripts/build.cmd
@@ -96,6 +96,7 @@ cd %collectionPath%
 del "%collectionName%.zip" /Q
 "c:\program files\7-zip\7z.exe" a "%collectionName%.zip"
 xcopy "%collectionName%.zip" "%deploymentFolderRoot%%versionNumber%" /Y
+xcopy "%collectionName%.zip" "c:\deployments\_current" /Y
 cd ..\..\scripts
 
 rem pause
