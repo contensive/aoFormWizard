0ad7dfb8b9fc68cec4ff7020991dc75a99368925
jkidwell <jay@contensive.com>
2025-06-02 22:47:58 -0400

    fix build

--- scripts/build.cmd
+++ scripts/build.cmd
@@ -96,7 +96,7 @@ cd %collectionPath%
 del "%collectionName%.zip" /Q
 "c:\program files\7-zip\7z.exe" a "%collectionName%.zip"
 xcopy "%collectionName%.zip" "%deploymentFolderRoot%%versionNumber%" /Y
-xcopy "%collectionName%.zip" "c:\deployments\_current" /Y
+xcopy "%collectionName%.zip" "c:\deployments\_current_sprint" /Y
 cd ..\..\scripts
 
 rem pause
